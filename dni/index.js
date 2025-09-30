539a2ef3b5fa657ceebe19eb40e16a82ee720d6f
fabian luna <fabianlv1920@gmail.com>
2025-09-30 09:53:32 +0200

    Calcular letra de DNI/NIE

---
+++ dni/index.js
@@ -0,0 +1,33 @@
+
+const cadena = "TRWAGMYFPDXBNJZSQVHLCKE"
+const letraNie="XYZ"
+
+function CalcularLetra() {
+    
+    let digitos = document.getElementById("digito").value
+    const letra=document.getElementById('letra')
+
+    if (!letraNie.includes(digitos[0]) ) {
+        console.log("DNI");
+
+        if (digitos.length!==8) {
+            letra.innerHTML="Numero invalido"
+        }
+        letra.innerHTML = cadena[parseInt(digitos) % 23] + " (DNI)"
+
+    } else {
+        console.log("NIE");
+
+        if (digitos.length==8) {
+           
+            digitos=letraNie.indexOf(digitos[0]) +digitos.slice(1) //Puedo usar substring()
+            console.log(digitos);
+            letra.innerHTML = cadena[parseInt(digitos)% 23] +" (NIE)"
+
+        }else{
+            letra.innerHTML="Numero invalido"
+        }
+
+    }
+
+}
